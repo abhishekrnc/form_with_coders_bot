a65ee82150c429695c9f057149bf0445120158f3
Abhishek <abhishektupudana@gmail.com>
2020-11-07 10:35:57 +0530

    First Commit

---
+++ contact-form-handler.php
@@ -0,0 +1,30 @@
+<?php
+$name = $_POST['name'];
+$visitor_email = $_POST['email'];
+$message = $_POST['message'];
+
+$email_from = "contact@abhishek.com";             <!-- Hey this is just a demo mail address -->
+
+$email_subject = "New Enquiry";
+
+$email_body = "User Name: $name.\n" .
+    "User Email: $visitor_email.\n" .
+    "User Message: $message.\n";
+
+$to = "abhishektupudana@gmail.com";
+
+$headers = "From: $email_from \r\n";
+
+$headers .= "Reply-to: $visitor_email \r\n";
+
+mail($to, $email_subject, $email_body, $headers);
+
+header("Location: index.html");
+
+
+?>
+
+
+<!-- Follow me on GitHub : https://github.com/abhishekrnc
+     Follow me on Twitter : https://twitter.com/abhishekrnc1
+     Follow me on LinkedIn : https://www.linkedin.com/in/abhishekrnc  -->
